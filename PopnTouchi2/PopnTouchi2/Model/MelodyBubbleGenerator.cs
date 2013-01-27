c10f6b7e51fc162ddad553a3501a2961849d546a
Aceltis <amandine.le-cahain@inse-rennes.fr>
2013-01-27 11:46:52 +0100

    Projet clean it 2

---
+++ PopnTouchi2/PopnTouchi2/Model/MelodyBubbleGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace PopnTouchi2
+{
+    public class MelodyBubbleGenerator : ObservableCollection<String> , INotifyPropertyChanged
+    {
+        private ObservableCollection<String> Bubbles;
+    }
+}
