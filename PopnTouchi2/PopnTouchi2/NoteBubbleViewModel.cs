5d0f7033c44464d05f2b7f2d3195506544405a6f
Denis Lemercier <denis.lemercier@insa-rennes.fr>
2013-02-04 16:51:11 +0100

    maj ViewModel

---
+++ PopnTouchi2/PopnTouchi2/NoteBubbleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PopnTouchi2
+{
+    public class NoteBubbleViewModel
+    {
+        private NoteBubble _noteBubble;
+
+        public event ContainerManipulationCompletedEventArgs dropBubble;
+    
+        public NoteBubble Note
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
