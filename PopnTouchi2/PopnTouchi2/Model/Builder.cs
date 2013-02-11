c7dcea85e0462a2442d1416bb80ab4bd7b896dc4
Adrien Brunelat <brunelat@crans.org>
2013-02-11 20:42:42 +0100

    added .cs

---
+++ PopnTouchi2/PopnTouchi2/Model/Builder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PopnTouchi2
+{
+    public class Builder
+    {
+        public void generateSession()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PopnTouchi2/PopnTouchi2/Model/Session.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PopnTouchi2
+{
+    public class Session
+    {
+        public Themes Themes
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public MelodyBubbleGenerator MelodyBubbleGenerator
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public NoteBubbleGenerator NoteBubbleGenerator
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public Stave Stave
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void reduce()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void enlarge()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
