1c16a64bc12ac628b7b9de611ec9e69dd6147578
Acemond <brunelat@crans.org>
2013-05-26 12:25:17 +0200

    note n notebubbles

--- PopnTouchi2/PopnTouchi2/ViewModel/NoteBubbleViewModel.cs
+++ PopnTouchi2/PopnTouchi2/ViewModel/NoteBubbleViewModel.cs
@@ -121,5 +121,64 @@ namespace PopnTouchi2.ViewModel
 
             Animation = new NoteBubbleAnimation(this, SessionVM);
         }
+
+        /// <summary>
+        /// NoteBubbleViewModel theme specific constructor.
+        /// </summary>
+        /// <param name="center">Location where the NoteBubble should be</param>
+        /// <param name="nb">The NoteBubble to link with its ViewModel</param>
+        /// <param name="sv">The Parent ScatterView</param>
+        /// <param name="s">The current SessionVM</param>
+        public NoteBubbleViewModel(Point center, NoteBubble nb, ScatterView sv, SessionViewModel s) : base(s)
+        {
+            NoteBubble = nb;
+            SVItem = new ScatterViewItem();
+            ParentSV = sv;
+
+            Random r = new Random();
+            SVItem.Center = center;
+
+            SVItem.CanScale = false;
+            SVItem.HorizontalAlignment = HorizontalAlignment.Center;
+            SVItem.CanRotate = false;
+            SVItem.HorizontalAlignment = HorizontalAlignment.Center;
+
+            FrameworkElementFactory bubbleImage = new FrameworkElementFactory(typeof(Image));
+            switch (SessionVM.Session.ThemeID)
+            {
+                case 1:
+                    bubbleImage.SetValue(Image.SourceProperty, new Theme1ViewModel(SessionVM.Session.Theme, SessionVM).GetNoteBubbleImageSource(nb.Note.Duration));
+                    break;
+                case 2:
+                    bubbleImage.SetValue(Image.SourceProperty, new Theme2ViewModel(SessionVM.Session.Theme, SessionVM).GetNoteBubbleImageSource(nb.Note.Duration));
+                    break;
+                case 3:
+                    bubbleImage.SetValue(Image.SourceProperty, new Theme3ViewModel(SessionVM.Session.Theme, SessionVM).GetNoteBubbleImageSource(nb.Note.Duration));
+                    break;
+                case 4:
+                    bubbleImage.SetValue(Image.SourceProperty, new Theme4ViewModel(SessionVM.Session.Theme, SessionVM).GetNoteBubbleImageSource(nb.Note.Duration));
+                    break;
+            }
+            bubbleImage.SetValue(Image.IsHitTestVisibleProperty, false);
+            bubbleImage.SetValue(Image.WidthProperty, 85.0);
+            bubbleImage.SetValue(Image.HeightProperty, 85.0);
+
+            FrameworkElementFactory touchZone = new FrameworkElementFactory(typeof(Ellipse));
+            touchZone.SetValue(Ellipse.FillProperty, Brushes.Transparent);
+            touchZone.SetValue(Ellipse.MarginProperty, new Thickness(15));
+
+            FrameworkElementFactory grid = new FrameworkElementFactory(typeof(Grid));
+            grid.AppendChild(bubbleImage);
+            grid.AppendChild(touchZone);
+
+            ControlTemplate ct = new ControlTemplate(typeof(ScatterViewItem));
+            ct.VisualTree = grid;
+
+            Style bubbleStyle = new Style(typeof(ScatterViewItem));
+            bubbleStyle.Setters.Add(new Setter(ScatterViewItem.TemplateProperty, ct));
+            SVItem.Style = bubbleStyle;
+
+            Animation = new NoteBubbleAnimation(this, SessionVM);
+        }
     }
 }
