32467f8dc2ba77ced55582788a81ea12afde4316
Acemond <brunelat@crans.org>
2013-05-27 21:07:57 +0200

    session's generation buttons

--- PopnTouchi2/PopnTouchi2/DesktopView.cs
+++ PopnTouchi2/PopnTouchi2/DesktopView.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Threading;
 using Microsoft.Surface.Presentation.Controls;
 using PopnTouchi2.ViewModel;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
 namespace PopnTouchi2
 {
@@ -34,6 +36,11 @@ namespace PopnTouchi2
         /// <summary>
         /// temporary
         /// </summary>
+        public SurfaceButton CreateDoubleSession_Button { get; set; }
+
+        /// <summary>
+        /// temporary
+        /// </summary>
         public SurfaceButton CreateSession_Button { get; set; }
 
         /// <summary>
@@ -44,13 +51,29 @@ namespace PopnTouchi2
             IDs = new List<int>();
 
             CreateSession_Button = new SurfaceButton();
-            CreateSession_Button.Width = 200;
-            CreateSession_Button.Height = 75;
-            CreateSession_Button.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
-            CreateSession_Button.VerticalAlignment = System.Windows.VerticalAlignment.Bottom;
-            CreateSession_Button.Content = "Generate new Session !!!";
+            CreateSession_Button.Width = 85;
+            CreateSession_Button.Height = 85;
+            CreateSession_Button.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            CreateSession_Button.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            CreateSession_Button.Margin = new Thickness(0.0, 0.0, 150.0, 150.0);
+            ImageBrush OnePButtonBG = new ImageBrush();
+            OnePButtonBG.ImageSource = new BitmapImage(new Uri(@"../../Resources/Images/ui_items/one_player.png", UriKind.Relative));
+            CreateSession_Button.Background = OnePButtonBG;
             CreateSession_Button.Click += new RoutedEventHandler(CreateSession_Button_Click);
+
+            CreateDoubleSession_Button = new SurfaceButton();
+            CreateDoubleSession_Button.Width = 125;
+            CreateDoubleSession_Button.Height = 125;
+            CreateDoubleSession_Button.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            CreateDoubleSession_Button.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            CreateDoubleSession_Button.Margin = new Thickness(150.0, 150.0, 0.0, 0.0);
+            ImageBrush TwoPButtonBG = new ImageBrush();
+            TwoPButtonBG.ImageSource = new BitmapImage(new Uri(@"../../Resources/Images/ui_items/two_players.png", UriKind.Relative));
+            CreateDoubleSession_Button.Background = TwoPButtonBG;
+            CreateDoubleSession_Button.Click += new RoutedEventHandler(CreateDoubleSession_Button_Click);
+
             Children.Add(CreateSession_Button);
+            Children.Add(CreateDoubleSession_Button);
 
             Photos = new ScatterView();
             Children.Add(Photos);
@@ -62,6 +85,17 @@ namespace PopnTouchi2
         /// <param name="sender"></param>
         /// <param name="e"></param>
         void CreateSession_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SessionVM = new SessionViewModel(ActualWidth, ActualHeight, new Session(), IDs);
+            Photos.Items.Add(SessionVM.SessionSVI);
+        }
+
+        /// <summary>
+        /// Event handling the Create Session Button Click Action.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void CreateDoubleSession_Button_Click(object sender, RoutedEventArgs e)
         {
             SessionVM = new SessionViewModel(ActualWidth, ActualHeight, new Session(), IDs);
             Photos.Items.Add(SessionVM.SessionSVI);
