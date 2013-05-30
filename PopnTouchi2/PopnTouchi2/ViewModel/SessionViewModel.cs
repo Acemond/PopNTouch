cf52e6a0907ae4abfa9b63f68fe3a667999ffa66
Acemond <brunelat@crans.org>
2013-05-30 19:04:16 +0200

    redcrox and scale locked bugfix

--- PopnTouchi2/PopnTouchi2/ViewModel/SessionViewModel.cs
+++ PopnTouchi2/PopnTouchi2/ViewModel/SessionViewModel.cs
@@ -595,6 +595,8 @@ namespace PopnTouchi2.ViewModel
             Animation = new SessionAnimation(this, true, desktop);
             Animation.Fs = ScStream;
             Reduced = true;
+
+            SessionSVI.CanScale = false;
         }
 
         /// <summary>
